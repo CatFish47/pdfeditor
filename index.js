f64985cd8e623255ac9483366eab82eb0541a18a
Grant Cheng <chengxgrant@gmail.com>
2020-05-27 22:08:37 -0700

    setup and install

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ var path = require('path');
 
 var mongoose = require('mongoose');
 
-var passport = require('passport');
+var fs = require('fs');
+
+var pdfjsLib = require("pdfjs-dist/build/pdf.js");
 
 var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1';
 
@@ -24,7 +26,7 @@ var server = http.createServer(app);
 /* Defines what port to use to listen to web requests */
 var port =  process.env.PORT
 						? parseInt(process.env.PORT):
-						4821;
+						8128;
 
 function startServer() {
 
@@ -37,57 +39,82 @@ function startServer() {
 		res.sendFile(filePath);
 	})
 
-	app.post('/', (req, res, next) => {
-		console.log(req.body);
-		res.send('OK');
+	app.get('/pdf', (req, res, next) => {
+		/* Any copyright is dedicated to the Public Domain.
+		 * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+		//
+		// Basic node example that prints document metadata and text content.
+		// Requires single file built version of PDF.js -- please run
+		// `gulp singlefile` before running the example.
+		//
+
+		// Loading file from file system into typed array
+		var pdfPath = req.body.file;
+
+		// Will be using promises to load document, pages and misc data instead of
+		// callback.
+		var loadingTask = pdfjsLib.getDocument(pdfPath);
+		loadingTask.promise
+		  .then(function (doc) {
+		    var numPages = doc.numPages;
+		    console.log("# Document Loaded");
+		    console.log("Number of Pages: " + numPages);
+		    console.log();
+
+		    var lastPromise; // will be used to chain promises
+		    lastPromise = doc.getMetadata().then(function (data) {
+		      console.log("# Metadata Is Loaded");
+		      console.log("## Info");
+		      console.log(JSON.stringify(data.info, null, 2));
+		      console.log();
+		      if (data.metadata) {
+		        console.log("## Metadata");
+		        console.log(JSON.stringify(data.metadata.getAll(), null, 2));
+		        console.log();
+		      }
+		    });
+
+		    var loadPage = function (pageNum) {
+		      return doc.getPage(pageNum).then(function (page) {
+		        console.log("# Page " + pageNum);
+		        var viewport = page.getViewport({ scale: 1.0 });
+		        console.log("Size: " + viewport.width + "x" + viewport.height);
+		        console.log();
+		        return page
+		          .getTextContent()
+		          .then(function (content) {
+		            // Content contains lots of information about the text layout and
+		            // styles, but we need only strings at the moment
+		            var strings = content.items.map(function (item) {
+		              return item.str;
+		            });
+		            console.log("## Text Content");
+		            console.log(strings.join(" "));
+		          })
+		          .then(function () {
+		            console.log();
+		          });
+		      });
+		    };
+		    // Loading of the first page will wait on metadata and subsequent loadings
+		    // will wait on the previous pages.
+		    for (var i = 1; i <= numPages; i++) {
+		      lastPromise = lastPromise.then(loadPage.bind(null, i));
+		    }
+		    return lastPromise;
+		  })
+		  .then(
+		    function () {
+		      console.log("# End of Document");
+		    },
+		    function (err) {
+		      console.error("Error: " + err);
+		    }
+		  );
+
+		res.send('OK')
 	})
-
-	app.get('/init.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './init.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/classes.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './classes.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/game.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './game.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/eventlisteners.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './eventlisteners.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/noisemap.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './noisemap.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/utils.js', (req, res, next) => {
-		var filePath = path.join(__dirname, './utils.js')
-		res.sendFile(filePath);
-	});
-
-	app.get('/login', (req, res, next) => {
-		var filePath = path.join(__dirname, './login.html');
-
-		res.sendFile(filePath);
-	})
-
-	app.get('/auth/facebook',
-	  passport.authenticate('facebook'));
-
-	app.get('/auth/facebook/callback',
-	  passport.authenticate('facebook', { failureRedirect: '/login' }),
-	  function(req, res) {
-
-		// Successful authentication, redirect home.
-    res.redirect('/');
-  });
 
 	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
 	server.on('listening', () => {
